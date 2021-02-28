8d74e4cae72cc6fdb53ffbb42d0b561099b0bf1e
Florent Buisson <florent.buisson.oreilly@maskedmails.com>
2021-02-28 08:32:22 -0600

    Adding jupyter notebook for chap 11

--- Chapter 11 - Mediation and Instrumental Variables/chap11 - analyzing data.py
+++ Chapter 11 - Mediation and Instrumental Variables/chap11 - analyzing data.py
@@ -6,28 +6,24 @@ Created on Sun May 10 11:44:30 2020
 """
 
 #################################
-##### This script analyzes the data used in chapter 12, 
+##### This script analyzes the data used in chapter 11, 
 ##### Mediation and Instrumental Variables
 #################################
 
 ##### Setup #####
 
 #Common libraries
-import os
 import pandas as pd
 from statsmodels.formula.api import ols
 import numpy as np
 
 
-#Loading the data from the chapter on moderation, then setting the right folder
-os.chdir('C:\\Users\\Florent\\Dropbox\\Synchronised\\Work_and_projects\\Behavioral data science book\\R scripts\\Part IV Advanced tools\\Chapter 11 - Introduction to moderation')
-hist_data_df = pd.read_csv('chap11-historical_data.csv')
-os.chdir('C:\\Users\\Florent\\Dropbox\\Synchronised\\Work_and_projects\\Behavioral data science book\\R scripts\\Part IV Advanced tools\\Chapter 12 - Mediation and Instrumental Variables')
+#Loading the data from the chapter on moderation
+hist_data_df = pd.read_csv('chap10-historical_data.csv')
 
 ##### 1. Mediation #####
 
-#### 1.1. Definition and Measurement ####
-
+#Regressions used in the text
 ols("duration~play_area", data=hist_data_df).fit().summary()
 ols("groceries_purchases~play_area", data=hist_data_df).fit().summary()
 ols("groceries_purchases~duration", data=hist_data_df).fit().summary()
@@ -67,27 +63,18 @@ def boot_CI_fun(dat_df, metric_fun, B = 100):
   return(confint)
 #boot_CI_fun(hist_data_df, percentage_mediated_fun)
 
-#### 1.4. Reducing Uncertainty in Coefficients ####
-
-# NEED MORE WORK HERE
-
 ##### 2. Instrumental variables #####
 
 #Common libraries
-import os
 import pandas as pd
 from statsmodels.formula.api import ols
 import numpy as np
 
-#Libraries for IV regressions
+#Library for IV regressions
 from linearmodels.iv import IV2SLS
 
-#Loading the data from the chapter 10, then setting the right folder
-
-os.chdir('C:\\Users\\Florent\\Dropbox\\Synchronised\\Work_and_projects\\Behavioral data science book\\R scripts\\Part III Experimental design\\Chapter 10 - cluster randomization and hierarchical modeling')
-hist_data_df = pd.read_csv('chap10-historical_data.csv')
+#Loading the experimental data from the chapter 9
 exp_data_df = pd.read_csv('chap10-experimental_data.csv')
-os.chdir('C:\\Users\\Florent\\Dropbox\\Synchronised\\Work_and_projects\\Behavioral data science book\\R scripts\\Part IV Advanced tools\\Chapter 12 - Mediation and Instrumental Variables')
 
 #Reformat group variable to binary in experimental data
 exp_data_df.group = np.where(exp_data_df.group == 'treat', 1, 0)
@@ -109,126 +96,3 @@ lm_mod.summary()
 iv_mod = IV2SLS.from_formula('M6Spend ~ 1 + age + reason + [call_CSAT ~ group]', 
                              exp_data_df).fit()
 iv_mod.params
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-hist_data_df['age_quart'] = pd.cut(hist_data_df['age'], 4, 
-                                   labels=['q4', 'q3', 'q2', 'q1'],
-                                   include_lowest=True)
-
-hist_data_df.groupby('play_area').agg(M = ('duration', lambda x: x.mean()),
-    SD = ('duration', lambda x: x.std()))
-                                     
-#### Multiple moderators ####
-
-# Measure coefficient for moderated moderation
-ols("duration~play_area * children * age", data=hist_data_df).fit().summary()
-
-# Determine 90%-CI for moderated moderation coefficient
-def metric_fun(dat_df):
-    model = ols("duration~play_area * children * age", data=dat_df)
-    res = model.fit(disp=0)
-    coeff = res.params['play_area:children:age']
-    return coeff
-#metric_fun(hist_data_df)
-
-
-
-#boot_CI_fun(hist_data_df, metric_fun)
-
-#### Validating moderation with Bootstrap ####
-
-#Changing the metric function
-def metric_fun2(dat_df):
-    model = ols("duration~play_area * children", data=dat_df)
-    res = model.fit(disp=0)
-    coeff = res.params['play_area:children']
-    return coeff
-metric_fun(hist_data_df)
-
-# Changing the bootstrap function
-def boot_CI_fun2(dat_df, metric_fun, B = 100):
-  #Setting the number of bootstrap samples
-  
-  #Setting sample size
-  N = 10000
-  conf_level = 0.9
-  coeffs = []
-  
-  
-  for i in range(B):
-      sim_data_df = dat_df.sample(n=N, replace = True)
-      coeff = metric_fun(sim_data_df)
-      coeffs.append(coeff)
-  
-  coeffs.sort()
-  start_idx = round(B * (1 - conf_level) / 2)
-  end_idx = - round(B * (1 - conf_level) / 2)
-  
-  confint = [coeffs[start_idx], coeffs[end_idx]]  
-  
-  return(confint)
-
-boot_CI_fun2(hist_data_df, metric_fun2, B = 1000)
-
-### Interpreting individual coefficients ###
-
-# Centering the age variable in the historical data
-centered_data_df = hist_data_df.copy()
-centered_data_df['age'] = centered_data_df['age'] \
-    .subtract(centered_data_df['age'].mean())
-
-ols("duration~play_area * age", data=hist_data_df).fit().summary()
-ols("duration~play_area * age", data=centered_data_df).fit().summary()
-
-# Changing the default for PlayArea to 1
-centered_data_df['play_area'] = centered_data_df['play_area']
-
-ols("duration~play_area * age", data=hist_data_df).fit().summary()
-ols("duration~play_area * age", data=centered_data_df).fit().summary()
-
-
-### Calculating effects at the level of business decisions
-
-def business_metric_fun(dat_df):
-    
-    model =  ols("duration~play_area * (children + age)", data=dat_df)
-    res = model.fit(disp=0)
-    
-    action_dat_df = dat_df[dat_df.play_area == 0].copy()
-    action_dat_df['pred_dur0'] = res.predict(action_dat_df) 
-    action_dat_df.play_area = 1
-    action_dat_df['pred_dur1'] = res.predict(action_dat_df)
-    action_dat_df['pred_dur_diff'] = \
-        action_dat_df.pred_dur1 - action_dat_df.pred_dur0
-    action_res_df = action_dat_df.groupby(['store_id']) \
-        .agg(mean_dur_diff=('pred_dur_diff', 'mean'), 
-             tot_dur_diff=('pred_dur_diff', 'sum'))
-    
-    return action_res_df
-
-action_res_df = business_metric_fun(hist_data_df)
-action_res_df.describe()
-
-
-
-
-
