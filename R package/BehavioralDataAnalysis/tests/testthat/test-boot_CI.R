d5a309fdf2c7c1f711cb6763b6af72268f4c6298
Florent Buisson <florent.buisson.oreilly@maskedmails.com>
2023-01-09 09:18:20 -0600

    Removing package stuff going to the other repo

--- R package/BehavioralDataAnalysis/tests/testthat/test-boot_CI.R
+++
@@ -1,6 +0,0 @@
-
-
-
-test_that("boot_CI returns an error when the number of Bootstrap simulations is too small in relation to the confidence level", {
-  expect_error(boot_CI(df_num, function(df) mean(df$x), B = 2), "the number of Bootstrap simulations is too small in relation to the confidence level")
-})
